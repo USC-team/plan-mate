bc368593527989c9591735c4cc2d9aa702406ee5
Aiman Yosofi <101520036+AimanYosofiUlt@users.noreply.github.com>
2025-06-03 09:29:01 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("jacoco")
 }
 
 group = "planmate"
@@ -18,4 +19,48 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+
+val includedPackages = listOf(
+    "**/data/**",
+    "**/domain/**",
+)
+
+val excludedPackages = listOf(
+    "**/dependencyInjection/**",
+    "**/domain/model/**",
+)
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+
+    classDirectories.setFrom(
+        fileTree(layout.buildDirectory.dir("classes/kotlin/main")) {
+            include(includedPackages)
+            exclude(excludedPackages)
+        }
+    )
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.test)
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.80".toBigDecimal()
+            }
+        }
+    }
+
+    classDirectories.setFrom(
+        fileTree(layout.buildDirectory.dir("classes/kotlin/main")) {
+            include(includedPackages)
+            exclude(excludedPackages)
+        }
+    )
 }
