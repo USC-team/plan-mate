af622e3396a93c98aec421fe6c2cd8f0fb9ca9af
Ala Ashowiter <olalcosh@gmail.com>
2025-06-11 23:56:38 +0300

    create login cli

--- src/main/kotlin/planmate/presentation/loginCli/LoginCLI.kt
+++ src/main/kotlin/planmate/presentation/loginCli/LoginCLI.kt
@@ -1,19 +1,26 @@
 package planmate.presentation.loginCli
 
 import planmate.domain.models.User
+import planmate.domain.usecase.FindUserUseCase
 
-class LoginCLI {
+class LoginCLI (private val findUserUseCase: FindUserUseCase){
     private var userName: String=""
     private lateinit var user:User
 
     fun login(){
-
+        try {
+            enterUserName()
+            findUser()
+        }
+        catch(e: Exception){
+            println("User is not valid!")
+        }
     }
     fun enterUserName(){
         println("Enter user name: ")
         userName=readln().trim()
     }
     fun findUser(){
-        
+        user=findUserUseCase.finUser(userName)
     }
 }
